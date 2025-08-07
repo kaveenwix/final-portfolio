0f3dfd7de65e0d0ee28fb24f36fe4cd10d4fa512
Paul <apaulries@gmail.com>
2025-08-07 10:00:47 -0700

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -23,6 +23,7 @@ export default defineConfig(({ mode }) => ({
       "@shared": path.resolve(__dirname, "./shared"),
     },
   },
+  base: process.env.VITE_BASE_PATH || "/final-portfolio",
 }));
 
 function expressPlugin(): Plugin {
